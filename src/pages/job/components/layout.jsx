9040addf473ad5162d8385eb67f317441083c225
MezianeKhalil <khalilmez2000@gmail.com>
2022-09-16 12:49:17 +0100

    build related section for job page

--- src/pages/job/components/layout.jsx
+++ src/pages/job/components/layout.jsx
@@ -4,16 +4,18 @@ import PropTypes from 'prop-types'
 
 import { Container } from '@components/_index'
 import { Navbar, Footer } from '@components/blocks/_index'
+import RelatedJobs from './related-jobs'
 
 export default function Layout ({ children }) {
   return (
     <div className="font-poppins flex flex-col min-h-screen">
       <Navbar withBg={true} />
       <div className="bg-gray-50 flex-grow">
-        <Container extraClasses="py-4 flex">
-          <main className="relative w-full lg:w-9/12">
+        <Container extraClasses="py-4 flex gap-4 relative">
+          <main className="relative w-full lg:w-8/12">
             {children}
           </main>
+          <RelatedJobs />
         </Container>
       </div>
       <Footer />
---
+++ src/pages/job/components/related-jobs.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function RelatedJobs () {
+  return (
+    <section className="sticky top-4 h-full z-20 w-4/12 bg-white border-2 border-gray-100 border-solid py-5 px-5 my-1">
+    <h2 className="text-2xl text-gray-900 mb-2">Find Related Jobs</h2>
+    <ul className="flex flex-col gap-2">
+      <li className="font-medium text-blue-500 text-lg">Senior NodeJS TypeScript Developer</li>
+      <li className="font-medium text-blue-500 text-lg">Senior NodeJS TypeScript Developer</li>
+      <li className="font-medium text-blue-500 text-lg">Senior NodeJS TypeScript Developer</li>
+      <li className="font-medium text-blue-500 text-lg">Senior NodeJS TypeScript Developer</li>
+      <li className="font-medium text-blue-500 text-lg">Senior NodeJS TypeScript Developer</li>
+    </ul>
+  </section>
+  )
+}
